a98abd33fa51bfaaa1d218be5ecbee21884392a2
unknown <xomdangvaisf@gmail.com>
2026-01-28 13:39:56 +0700

    feat: API [User] implement login, register

--- services/auth-service/src/main/java/pnvteck/auth/dto/AuthRequestLogin.java
+++ services/auth-service/src/main/java/pnvteck/auth/dto/AuthRequestLogin.java
@@ -10,6 +10,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthRequestLogin {
-    private String username;
+    private String email;
     private String password;
 }
