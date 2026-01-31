8e8575da566efd9b64de9264ccbef2532e892f42
unknown <xomdangvaisf@gmail.com>
2026-01-31 21:50:24 +0700

    feature: API [User] implement logout

--- services/common-lib/src/main/java/pnvteck/common/exception/GlobalExceptionHandler.java
+++ services/common-lib/src/main/java/pnvteck/common/exception/GlobalExceptionHandler.java
@@ -1,4 +1,24 @@
 package pnvteck.common.exception;
 
-public class GlobalExceptionHandler {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import pnvteck.common.response.ApiResponse;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Object>> handleException(Exception e) {
+        log.error("Exception: ", e);
+        return ResponseEntity.internalServerError().body(
+                ApiResponse.builder()
+                        .code(1000)
+                        .message("Internal server error")
+                        .build()
+        );
+    }
 }
+
